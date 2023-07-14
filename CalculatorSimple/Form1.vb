31dba1d6422c0fad44b46ffbd1ab381f45e4fc2f
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-07-14 23:49:45 +0700

    Initial commit

--- CalculatorSimple/Form1.vb
+++ CalculatorSimple/Form1.vb
@@ -1,34 +1,36 @@
 ﻿Public Class Form1
 	Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
-		Dim angka1 As Int32 = Int32.Parse(txt1.Text)
-		Dim angka2 As Int32 = Int32.Parse(txt2.Text)
-		Dim hasil As Int32 = angka1 + angka2
+		Dim angka1 As Double = Double.Parse(txt1.Text)
+		Dim angka2 As Double = Double.Parse(txt2.Text)
+		Dim hasil As Double = angka1 + angka2
 
 		txtHasil.Text = hasil.ToString()
 	End Sub
 
 	Private Sub btnKurang_Click(sender As Object, e As EventArgs) Handles btnKurang.Click
-		Dim angka1 As Int32 = Int32.Parse(txt1.Text)
-		Dim angka2 As Int32 = Int32.Parse(txt2.Text)
-		Dim hasil As Int32 = angka1 - angka2
+		Dim angka1 As Double = Double.Parse(txt1.Text)
+		Dim angka2 As Double = Double.Parse(txt2.Text)
+		Dim hasil As Double = angka1 - angka2
 
 		txtHasil.Text = hasil.ToString()
 	End Sub
 
 	Private Sub btnKali_Click(sender As Object, e As EventArgs) Handles btnKali.Click
-		Dim angka1 As Int32 = Int32.Parse(txt1.Text)
-		Dim angka2 As Int32 = Int32.Parse(txt2.Text)
-		Dim hasil As Int32 = angka1 * angka2
+		Dim angka1 As Double = Double.Parse(txt1.Text)
+		Dim angka2 As Double = Double.Parse(txt2.Text)
+		Dim hasil As Double = angka1 * angka2
 
 		txtHasil.Text = hasil.ToString()
+		MsgBox(hasil)
 	End Sub
 
 	Private Sub btnBagi_Click(sender As Object, e As EventArgs) Handles btnBagi.Click
-		Dim angka1 As Int32 = Int32.Parse(txt1.Text)
-		Dim angka2 As Int32 = Int32.Parse(txt2.Text)
-		Dim hasil As Int32 = angka1 / angka2
+		Dim angka1 As Double = Double.Parse(txt1.Text)
+		Dim angka2 As Double = Double.Parse(txt2.Text)
+		Dim hasil As Double = angka1 / angka2
 
 		txtHasil.Text = hasil.ToString()
+		MessageBox.Show(hasil)
 	End Sub
 
 	Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
